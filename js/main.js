8cfc3d14a16cecd7266ae7734e4dca72a603334d
Nathaniel <nathanielcoronacion3@gmail.com>
2026-09-18 22:34:19 +0800

    feat: fixed css issue and combined all codes into one giant html file

--- js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { WalletStore } from './WalletStore.js';
-import { WalletUI } from './WalletUI.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const store = new WalletStore();
-  window.walletApp = new WalletUI(store);
-});
